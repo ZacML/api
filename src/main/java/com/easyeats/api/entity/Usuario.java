c941207b1381dd8bad045f3502d0420d044ed855
joao-vitor1007 <joaovitorbatista0104@gmail.com>
2026-09-14 21:45:26 -0300

    entidade e mapper

--- src/main/java/com/easyeats/api/entity/Usuario.java
+++ src/main/java/com/easyeats/api/entity/Usuario.java
@@ -2,13 +2,18 @@ package com.easyeats.api.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import javax.management.relation.Role;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "tb_usuario")
 public class Usuario {
 
